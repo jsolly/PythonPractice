e8ca27bc26580abb6f6f039d2908740003480e75
john8739 <jsolly@esri.com>
2020-10-15 15:12:56 -0700

    wip

--- selemium_tricks.py
+++ selemium_tricks.py
@@ -4,4 +4,10 @@ el = driver.find_element_by_xpath("//*[text()='Exact Match']")
 el = driver.find_element_by_xpath("//*[contains(text(), 'Partial Match')]")
 
 loginButton = browser.find_element(By.XPATH, "//*[@value='Log In']")
+
+
+self.assertTrue(
+                "You do not have permissions"
+                in self.driver.find_elements_by_tag_name("p")[1].text
+            )
 """
